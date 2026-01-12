98aa4cba247d86288fb09330324718acaa919bf5
HritzWalunj <hritujawalunj@gmail.com>
2026-01-12 17:26:28 +0530

    add comom workflow template and fixed pylint errors

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,19 +1,25 @@
+"""import packages"""
 import pytest
 from calculator.calculator import add, subtract, multiply, divide
 
 
 def test_add():
+    """test add function"""
     assert add(2,3) == 5
 
 def test_subtract():
+    """test substact function"""
     assert subtract(3,2) == 1
 
 def test_multiply():
+    """test multiply function"""
     assert multiply(3,2) == 6
 
 def test_divide():
+    """test  divide function"""
     assert divide(6,2) == 3
 
 def test_divide_by_zero():
+    """test divede_by_zero function"""
     with pytest.raises(ValueError):
         divide(10,0)
